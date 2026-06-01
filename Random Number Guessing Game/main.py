ad447e8a0834817d1bd275f9be66a46a64eeed78
pelfa145 <cymig213@gmail.com>
2026-06-02 00:46:58 +0800

    Changes and improvements to random number guessing game (added a replay system)

--- Random Number Guessing Game/main.py
+++ Random Number Guessing Game/main.py
@@ -3,24 +3,37 @@ import random
 input("Guessing Game\nPress anything to view the rules..")
 input("I generate a number and you try to guess whether its higher or lower")
 attempts=0
-number=random.randint(1, 100)
-print("Number is between 1-100")
-while True:
-    guess=input("Guess: ")
-    try:
-        guess=int(guess)
-    except:
-        print("Error: That is not a valid number. I need a digit.")
-        continue
+number=0
 
-    attempts+=1
-    if guess < number:
-        print("Higher!")
-    elif guess > number:
-        print("Lower!")
-    else:
-        print(f"Congrats! the correct answer is {number}")
-        input(f"you got it in {attempts} attempts!")
-        break
+while True:
+    attempts=0
+    number=random.randint(1, 100)
+    print("Number is between 1-100")
+    while True:
+        guess=input("Guess: ")
+        try:
+            guess=int(guess)
+        except:
+            print("Error: That is not a valid number. I need a digit.")
+            continue
+
+        attempts+=1
+        if guess < number:
+            print("Higher!")
+        elif guess > number:
+            print("Lower!")
+        else:
+            print(f"Congrats! the correct answer is {number}")
+            input(f"you got it in {attempts} attempts!")
+            break
+    
+    userInput2=input("you wanna play again? Y/n: ")
+    if userInput2 == "y":
+                continue
+    elif userInput2=="n":
+                break
+    
+    elif userInput2=="":
+                continue
 
 
